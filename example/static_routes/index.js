dd17a4a4590d0a97737f1a89f0f7037055356fb5
Maxime Maillet <maxime.maillet93@gmail.com>
2019-07-23 15:27:05 +0200

    refacto example

--- example/static_routes/index.js
+++ example/static_routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const router = require('../../index');
+const router = require('express-imp-router');
 
 router(app);
 router.enableDebug();
 router.route([
 	{
+	  controllers: 'useless',
 		routes: {
 			'/public': {
 				[router.IMP.STATIC]: {
